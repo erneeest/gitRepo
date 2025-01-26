595aedd69fcafc4de55731fa5a89926da5b55040
Ernest Talagtag <ernestneil09@gmail.com>
2025-01-26 15:54:02 +0800

    making a rock paper scissor

---
+++ script.js
@@ -0,0 +1,11 @@
+function result(computerMove, userMove){
+
+    alert(`Your move is ${userMove}, Computer move is ${computerMove}`);
+
+}
+function computerMove(){
+
+    const computerMove = '';
+    const probability = Math.random();
+    
+}
