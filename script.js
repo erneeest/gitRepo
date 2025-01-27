466b11076715274b0dba17d0dcb350919a113e72
Ernest Talagtag <ernestneil09@gmail.com>
2025-01-27 12:59:04 +0800

    fixed bugs in the scoring part, (need to store the scores locally)

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-
 const score = {
     wins: 0,
     losses: 0,
     draws: 0
 }
 
-const alertResult ={
-    draw: alert(`Your move is ${userMove}, Computer move is ${computerMove}\nTie\nwins: ${score.wins}, losses: ${score.losses}, wins: ${score.draws}`),
-    win: alert(`Your move is ${userMove}, Computer move is ${computerMove}\nYou Win\nwins: ${score.wins}, losses: ${score.losses}, wins: ${score.draws}`),
-    losses: alert(`Your move is ${userMove}, Computer move is ${computerMove}\nYou Lose\nwins: ${score.wins}, losses: ${score.losses}, wins: ${score.draws}`)
+function UserMove(userMove){
+    const computer = ComputerMove();
+
+    Compare(userMove, computer);
 }
 
-
-function userMove(userMove){
-    const computer = computerMove();
-
-    compare(userMove, computer);
-}
-
-function computerMove(){
+function ComputerMove(){
     let compMove = '';
     const random = Math.random();
 
@@ -33,35 +25,42 @@ function computerMove(){
     return compMove;
 }
 
-function compare(userMove, computerMove){
+function Compare(userMove, computerMove){
     if(userMove === computerMove){
         score.draws++;
-        alertResult.draw
+        DrawFunction(userMove, computerMove);
     }else if(userMove === 'Paper' && computerMove === 'Rock'){
         score.wins++;
-        alertResult.win;
-
+        WinFunction(userMove, computerMove);
     }else if(userMove === 'Rock' && computerMove === 'Scissor'){
         score.wins++;
-        alertResult.win;
-
+        WinFunction(userMove, computerMove);
     }else if(userMove === 'Scissor' && computerMove === 'Paper'){
         score.wins++;
-        alertResult.win;
-
+        WinFunction(userMove, computerMove);
     }
     
     else if(userMove === 'Paper' && computerMove === 'Scissor'){
         score.losses++;
-        alertResult.losses;
+        LossesFunction(userMove, computerMove);
 
     }else if(userMove === 'Scissor' && computerMove === 'Rock'){
         score.losses++;
-        alertResult.losses;
+        LossesFunction(userMove, computerMove);
 
     }else if(userMove === 'Rock' && computerMove === 'Paper'){
         score.losses++;
-        alertResult.losses;
+        LossesFunction(userMove, computerMove);
 
     }
+}
+
+function WinFunction(userMove, computerMove){
+    alert(`Your move is ${userMove}, Computer move is ${computerMove}\nTie\nwins: ${score.wins}, losses: ${score.losses}, draws: ${score.draws}`);
+}
+function LossesFunction(userMove, computerMove){
+    alert(`Your move is ${userMove}, Computer move is ${computerMove}\nYou Lose\nwins: ${score.wins}, losses: ${score.losses}, draws: ${score.draws}`);
+}
+function DrawFunction(userMove, computerMove){
+    alert(`Your move is ${userMove}, Computer move is ${computerMove}\nTie\nwins: ${score.wins}, losses: ${score.losses}, draws: ${score.draws}`);
 }
